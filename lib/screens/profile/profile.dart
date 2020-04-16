db130fc8fdea846e2a6bc7265823b05064edb595
Alexius22 <alexios1@hotmail.it>
2020-04-16 19:31:59 +0200

    Implemented the profile and settings sections

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -2,11 +2,155 @@
 
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Profile"),
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width / 3.1, top: 5),
+                child: Text(
+                  "Account",
+                  style: TextStyle(
+                    fontFamily: "OpenSans",
+                    fontWeight: FontWeight.w600,
+                    fontSize: MediaQuery.of(context).size.height / 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 40,
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width / 5,
+                    bottom: MediaQuery.of(context).size.width / 30),
+                child: Container(
+                  width: MediaQuery.of(context).size.height / 7,
+                  height: MediaQuery.of(context).size.height / 7,
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange[900],
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('images/profile.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                    border: Border.all(color: Colors.white, width: 2.0),
+                  ),
+                ),
+              ),
+              SizedBox(width: MediaQuery.of(context).size.height / 35),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Mario Verdi",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: MediaQuery.of(context).size.height / 35,
+                      ),
+                    ),
+                    Text(
+                      "Perugia (PG)",
+                      style: TextStyle(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          ListTile(
+            title: Text(
+              "Password",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+            subtitle: Text(
+              "********",
+              style: TextStyle(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            trailing: Icon(
+              Icons.keyboard_arrow_right,
+              color: Colors.grey.shade400,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text(
+              "Email",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+            subtitle: Text(
+              "prova@example.com",
+              style: TextStyle(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            trailing: Icon(
+              Icons.keyboard_arrow_right,
+              color: Colors.grey.shade400,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text(
+              "Pagamento",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+            subtitle: Text(
+              "Visa xxxx-xxxx-xxxx-1234",
+              style: TextStyle(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            trailing: Icon(
+              Icons.keyboard_arrow_right,
+              color: Colors.grey.shade400,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text(
+              "Logout",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -2,11 +2,122 @@
 
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Settings"),
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 15, top: 5),
+                child: Text(
+                  "Impostazioni",
+                  style: TextStyle(
+                    fontFamily: "OpenSans",
+                    fontWeight: FontWeight.w600,
+                    fontSize: MediaQuery.of(context).size.height / 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 100,
+          ),
+          ListTile(
+            title: Text(
+              "Lingua",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+            subtitle: Text("Italiano IT",
+                style: TextStyle(
+                  color: Colors.grey.shade400,
+                )),
+            trailing: Icon(
+              Icons.keyboard_arrow_right,
+              color: Colors.grey.shade400,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text(
+              "Posizione",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+            subtitle: Text("Perugia",
+                style: TextStyle(
+                  color: Colors.grey.shade400,
+                )),
+            trailing: Icon(
+              Icons.keyboard_arrow_right,
+              color: Colors.grey.shade400,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text(
+              "Filtri",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+            subtitle: Text("Nessuno",
+                style: TextStyle(
+                  color: Colors.grey.shade400,
+                )),
+            trailing: Icon(
+              Icons.keyboard_arrow_right,
+              color: Colors.grey.shade400,
+            ),
+            onTap: () {},
+          ),
+          SwitchListTile(
+            title: Text("Vietato Minori",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.height / 40,
+                )),
+            subtitle: Text("Off",
+                style: TextStyle(
+                  color: Colors.grey.shade400,
+                )),
+            value: false,
+            onChanged: (val) {},
+          ),
+          SwitchListTile(
+            title: Text("Notifiche Push",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.height / 40,
+                )),
+            subtitle: Text("On",
+                style: TextStyle(
+                  color: Colors.grey.shade400,
+                )),
+            value: true,
+            onChanged: (val) {},
+          ),
+        ],
+      ),
     );
   }
 }
