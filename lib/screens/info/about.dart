3f502d94256fa89580c14e080780e078a6f5df80
Alexius22 <alexios1@hotmail.it>
2020-04-30 17:20:40 +0200

    Reworked the info section with further details and a section about us with button for the website. Updated the version of url_launcher

---
+++ lib/screens/info/about.dart
@@ -0,0 +1,84 @@
+// Copyright 2020 Amatucci & Strippoli. All rights reserved.
+
+import 'package:flutter/material.dart';
+
+// Widget
+import 'package:cinema_app/widgets/appbars/go_back_appbar.dart';
+import 'package:cinema_app/widgets/buttons/button_icon.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AboutCinema extends StatelessWidget {
+  _openSiteweb() async {
+    String url = "https:<URL>";
+    if (await canLaunch(url)) await launch(url);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: GoBackAppBar("Chi siamo?").build(context),
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width / 40,
+                right: MediaQuery.of(context).size.width / 40),
+            child: Container(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: <Widget>[
+                    _image(context, 'images/cinema.jpg'),
+                    _whoweare(context),
+                    _buttonSiteweb(context, "Sito web"),
+                    SizedBox(height: MediaQuery.of(context).size.height / 80)
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _image(context, image) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      child: Image(
+        image: AssetImage(image),
+      ),
+    );
+  }
+
+  Widget _whoweare(context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet ultrices quam. Suspendisse interdum enim vitae odio laoreet, pulvinar vulputate eros tempor. Nulla dignissim venenatis quam a auctor. Etiam mauris nisl, consectetur varius est vitae, vulputate tincidunt sem. Sed dolor neque, tincidunt nec velit sit amet, varius ornare felis. Aenean scelerisque nunc quis quam ornare, in vestibulum dolor volutpat. Quisque sed nunc ut est varius convallis vel ut magna. Ut a convallis velit. In dapibus mi ac elit dictum, in ornare purus finibus. Vestibulum ac urna nec odio congue tincidunt. Curabitur mauris mauris, eleifend vel ex molestie, viverra venenatis augue. Nam eget sollicitudin orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec ac mi tincidunt, cursus enim quis, dignissim erat. In viverra auctor sagittis. Curabitur ornare congue felis.",
+            style: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: MediaQuery.of(context).size.height / 45,
+              letterSpacing: 1,
+            ),
+            textAlign: TextAlign.justify,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buttonSiteweb(context, text) {
+    return Padding(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 50),
+      child: ButtonWithIcon(
+        width: MediaQuery.of(context).size.width / 2.8,
+        text: text,
+        icon: Icons.language,
+        onTap: _openSiteweb,
+      ),
+    );
+  }
+}
--- lib/screens/info/info_cinema.dart
+++ lib/screens/info/info_cinema.dart
@@ -1,9 +1,12 @@
 // Copyright 2020 Amatucci & Strippoli. All rights reserved.
 
+import 'package:cinema_app/transitions/slide_top_route.dart';
 import 'package:flutter/material.dart';
 
 // Widget
 import 'package:cinema_app/widgets/buttons/button_icon.dart';
+
+import 'package:cinema_app/screens/info/about.dart';
 
 class InfoCinema extends StatelessWidget {
   @override
@@ -11,7 +14,6 @@ class InfoCinema extends StatelessWidget {
     return Container(
       height: MediaQuery.of(context).size.height,
       child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
         scrollDirection: Axis.vertical,
         child: Padding(
           padding: EdgeInsets.only(
@@ -19,14 +21,39 @@ class InfoCinema extends StatelessWidget {
               right: MediaQuery.of(context).size.width / 40),
           child: Column(
             children: <Widget>[
-              _buildInfo(context, "Ecco dove si trova il cinema selezionato:",
-                  "Via Fasulla, 1, 06123, Perugia (PG)"),
+              _buildTitle2(
+                  context, "Ecco dove si trova il cinema selezionato:"),
+              // TODO: Cambiare la città in alto dovrebbe cambiare anche l'indirizzo
+              _buildInfo2(context, "Via Fasulla, 1, 06123, Perugia (PG)"),
               _map(context, 'images/map.png'),
               _directions(context, "Ottieni indicazioni", null),
-              _buildInfo(context, "Numero telefonico:", "0123456789"),
-              _buildInfo(context, "Orari Apertura:",
-                  "Giorni feriali:\n16:00 ~ 00:00\nGiorni festivi:\n15:00 ~ 01:00"),
-              _siteweb(context, "Sito Web", null),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      _buildTitle(context, "Orari Apertura"),
+                      //SizedBox(width: 50),
+                      _buildTitle(context, "Costo biglietti"),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      _buildInfo(context,
+                          "GIORNI FERIALI:\n16:00 ~ 00:00\n\nGIORNI FESTIVI:\n15:00 ~ 01:00"),
+                      _buildInfo(context,
+                          "2D:\nIntero: €7.00\nRidotto €5.00\n3D:\nIntero: €10.00\nRidotto €7.00\n"),
+                    ],
+                  ),
+                ],
+              ),
+              _buildTitle2(context, "Numero telefonico"),
+              _buildInfo2(context, "0123456789"),
+              _about(context, "Chi siamo"),
+              SizedBox(height: MediaQuery.of(context).size.height / 40),
             ],
           ),
         ),
@@ -34,23 +61,16 @@ class InfoCinema extends StatelessWidget {
     );
   }
 
-  Widget _buildInfo(context, description, info) {
+  Widget _buildTitle(context, description) {
     return Container(
       child: Padding(
-        padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 50),
+        padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height / 50,
+            left: MediaQuery.of(context).size.width / 17),
         child: Column(
           children: <Widget>[
             Text(
               description,
-              style: TextStyle(
-                fontFamily: 'OpenSans',
-                fontSize: MediaQuery.of(context).size.height / 45,
-                letterSpacing: 1,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              info,
               style: TextStyle(
                 fontFamily: 'OpenSans',
                 fontWeight: FontWeight.bold,
@@ -61,6 +81,64 @@ class InfoCinema extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildInfo(context, info) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.only(left: 35),
+        child: Column(
+          children: <Widget>[
+            Text(
+              info,
+              style: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: MediaQuery.of(context).size.height / 45,
+                letterSpacing: 1,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle2(context, description) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(
+            description,
+            style: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: MediaQuery.of(context).size.height / 40,
+              letterSpacing: 1,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfo2(context, info) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text(
+            info,
+            style: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: MediaQuery.of(context).size.height / 45,
+              letterSpacing: 1,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
@@ -83,14 +161,21 @@ class InfoCinema extends StatelessWidget {
     );
   }
 
-  Widget _siteweb(context, text, func) {
+  Widget _about(context, text) {
     return Padding(
       padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 50),
       child: ButtonWithIcon(
-        width: MediaQuery.of(context).size.width / 2.8,
+        width: MediaQuery.of(context).size.width / 2.6,
         text: text,
-        icon: Icons.language,
-        onTap: (func) {},
+        icon: Icons.help_outline,
+        onTap: () {
+          Navigator.push(
+            context,
+            SlideTopRoute(
+              page: AboutCinema(),
+            ),
+          );
+        },
       ),
     );
   }
